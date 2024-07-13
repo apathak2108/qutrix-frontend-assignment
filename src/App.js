4946baaec29f49df21450e5960d46cb59db2cf36
Anany Pathak <propanany@gmail.com>
2024-07-13 23:46:14 +0530

    routing and header compoment suceessfully made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Header from "./components/header";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pie-chart" />
+        <Route path="/line-chart" />
+        <Route path="/bar-chart" />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/home/home.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledMainContainer = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-flow: column;
+    background-color: #fff;
+    ;
+`;
+
+export const StyledUpperDiv = styled.section`
+    width: 100%;
+    
+`;
+
